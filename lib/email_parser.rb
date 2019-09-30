9363fd32b20e00f4c57425ed9ec7dc5ff19f668a
Naya  Willis <greedybrain18@gmail.com>
2019-09-30 23:20:22 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,14 @@ class EmailAddressParser
   end
   
   def parse
-    
+    test = "greedybrain18@gmail.com willisnaya@gmail.com, hottechandchill@gmail.com"
+
+consider_it_parsed = test.split(' ').collect do |val|
+  if val[-1] == ',' || val[-1] == ' '
+    val = val[0...-1]
+  end
+  val
+end
   end
 
 end
